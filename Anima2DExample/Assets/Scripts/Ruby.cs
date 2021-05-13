c7ebbcc9ff147217a8860dea6586f03c76d20c38
SofiaL911 <83292332+SofiaL911@users.noreply.github.com>
2021-05-12 20:05:20 -0500

    Platform and Enemy

--- Anima2DExample/Assets/Scripts/Ruby.cs
+++ Anima2DExample/Assets/Scripts/Ruby.cs
@@ -6,18 +6,20 @@ public class Ruby : MonoBehaviour
 {
     public Animator animator;
     public SpriteRenderer ruby;
+    public Rigidbody2D rigidbody;
+
     [Header("Balance variables")]
     [SerializeField]
     private float moveSpeed;
-    
-    
+    [SerializeField]
+    private float jumpForce = 1; 
+
     public int currentHP = 30;
-   
     public int HP = 30;
 
-
-    //private int HP = 30;
-    // private int currentHP = 30; Esta en el Script Hazard
+    private float horizontal;
+    private float vertical;
+    private Vector3 direction;
 
 
     // Start is called before the first frame update
@@ -29,8 +31,12 @@ public class Ruby : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        direction = new Vector3(horizontal, 0f, vertical);
+        #region Unused
         //w -> Back
-        if (Input.GetKey(KeyCode.W))
+        /*if (Input.GetKey(KeyCode.W))
         {
             animator.SetBool("RunBack",true);
             animator.SetBool("RunFront",false);
@@ -45,14 +51,22 @@ public class Ruby : MonoBehaviour
             animator.SetBool("RunSide", false);
             transform.position = new Vector2(transform.position.x, transform.position.y - moveSpeed);
         }
+        */
+        #endregion
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector2(0f, jumpForce),ForceMode2D.Impulse);
+        }
+
         //a -> Left
 
         if (Input.GetKey(KeyCode.A))
         {
             ruby.flipX = false;
 
-            animator.SetBool("RunBack", false);
-            animator.SetBool("RunFront", false);
+            //animator.SetBool("RunBack", false);
+            //animator.SetBool("RunFront", false);
             animator.SetBool("RunSide", true);
             transform.position = new Vector2(transform.position.x - moveSpeed, transform.position.y);
         }
@@ -62,12 +76,16 @@ public class Ruby : MonoBehaviour
         {
             ruby.flipX = true;
 
-            animator.SetBool("RunBack", false);
-            animator.SetBool("RunFront", false);
+            //animator.SetBool("RunBack", false);
+            //animator.SetBool("RunFront", false);
             animator.SetBool("RunSide", true);
             transform.position = new Vector2(transform.position.x + moveSpeed, transform.position.y);
         }
+        if(direction.magnitude == 0f)
+        {
+            animator.SetBool("RunSide", false);
 
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -89,7 +107,15 @@ public class Ruby : MonoBehaviour
             //activar heal particles 
 
         }
-
+        if (collision.CompareTag("Enemy"))
+        {
+            if ((currentHP - collision.GetComponent<Enemy>().DamageInflict) < 0)
+                currentHP = 0;
+            else
+                currentHP -= collision.GetComponent<Enemy>().DamageInflict;
+            animator.SetTrigger("HitSide");
+        }
     }
+
 
 }
